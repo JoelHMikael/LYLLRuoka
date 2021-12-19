e5bd4fe003b5e8478da3f8a44d95acfcfb1267ac
JoelHMikael <joel.h.kronqvist@gmail.com>
2021-12-19 19:55:51 +0200

    More fixes to the running

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Put in this dir so that it can execute:
 cd /home/joel
+rm -r -f ./FoodJS
 git clone "https://github.com/JoelHMikael/FoodJS.git" >> /tmp/test.txt
 node ./FoodJS/server.js
