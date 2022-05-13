b05d15e4e2771b31881fe8f0cf4c1deb8a2f4b58
Joel Kronqvist <work.joelkronqvist@pm.me>
2022-05-13 16:39:10 +0300

    Fixed big bad bug.

--- init.sh
+++ init.sh
@@ -5,15 +5,15 @@ echo | date
 echo "Init running"
 
 echo "Removing old packages..."
-rm -rf "${PATH_TO_LYLLRUOKA%%FoodJS}"
-
-cd "${PATH_TO_LYLLRUOKA%%FoodJS}"
+rm -rf $PATH_TO_LYLLRUOKA
 
 echo "Waiting for connection..."
 while [ ! "$(ping 'www.github.com' -c 1)" ]
 do
         sleep 5
 done
+
+cd "${PATH_TO_LYLLRUOKA%%FoodJS}"
 
 echo "Cloning new packages..."
 git clone "https://github.com/JoelHMikael/FoodJS.git"
